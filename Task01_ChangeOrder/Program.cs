144c57fdd5535effb0e91f50011a72e93c604848
NKompa <nkompa7@gmail.com>
2022-09-17 13:10:09 +0300

    Sort elements from max to min

--- Task01_ChangeOrder/Program.cs
+++ Task01_ChangeOrder/Program.cs
@@ -21,20 +21,21 @@ void SortMatrix (int [,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            int maxIndex = j;
             for (int k = j+1; k < array.GetLength(1); k++)
             {
-                if(array[i,k] > array[i,maxIndex]) maxIndex=k;
+                if(array[i,k] > array[i,j])
+                {
+                    int temp = array[i,k];
+                    array[i,k] = array[i,j];
+                    array[i,j] = temp;
+                }
             }
-            int temp = array[i,j];
-            array[i,j] = array[i,maxIndex];
-            array[i,maxIndex] = temp;
             Console.Write(array[i,j]+"\t");
         }
         Console.WriteLine();
     }
 }
 
-int [,] newArray = GetMatrix(4,4,1,21);
+int [,] newArray = GetMatrix(4,5,-99,100);
 Console.WriteLine();
 SortMatrix(newArray);
